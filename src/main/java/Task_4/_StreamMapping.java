b17e34546985325d3fa496bebdf08117dccf44c5
Gabrielle Johnson <gajohnson@griddynamics.com>
2025-10-08 09:37:36 -0500

    Adding task 4 and task 5

--- src/main/java/Task_4/_StreamMapping.java
+++ src/main/java/Task_4/_StreamMapping.java
@@ -11,11 +11,11 @@ public class _StreamMapping {
     public static void main(String[] args) {
 
 
-        List<Integer> list1 = Arrays.asList(1,2,3);
-        List<Integer> list2 = Arrays.asList(4,5,6);
-        List<Integer> list3 = Arrays.asList(7,8,9);
+        List<Integer> list1 = Arrays.asList(1, 2, 3);
+        List<Integer> list2 = Arrays.asList(4, 5, 6);
+        List<Integer> list3 = Arrays.asList(7, 8, 9);
 
-        List<Integer> nestedList =Stream.of(list1, list2,list3)
+        List<Integer> nestedList = Stream.of(list1, list2, list3)
                 .flatMap(List::stream)
                 .toList();
 
@@ -29,12 +29,21 @@ public class _StreamMapping {
 
         System.out.println("Combined 2D String: " + arrayAlpha);*/
 
-       List <String> arrayAlpha = Arrays.stream(dataArray)
+        List<String> arrayAlpha = Arrays.stream(dataArray)
                 .flatMap(Arrays::stream)
-               .toList();
+                .toList();
 
         System.out.println("Combined 2D String: " + arrayAlpha);
 
+        ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(1, 1, 2, 3, 3, 3, 4, 5, 6, 6, 6, 7));
+
+        List <Integer> distinctList = numbersList.stream()
+                .distinct()
+                .toList();
+
+        System.out.println("Distinct Numbers: "+ distinctList);
+
+        //ArrayList<Integer> numbersLists = new ArrayList<>(Arrays.asList(1, 1, 2, 3, 3, 3, 4, 5, 6, 6, 6, 7));
 
     }
 }
---
+++ src/main/java/Task_5/_StreamList.java
@@ -0,0 +1,25 @@
+package Task_5;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class _StreamList {
+
+    public static void main(String[] args) {
+        Map<String, List<String>> people = new HashMap<>();
+        people.put("John", Arrays.asList("555-1123","s", "555-3389", "a"));
+        people.put("Mary", Arrays.asList("555-2243","z", "555-5264"));
+        people.put("Steve", Arrays.asList("555-6654", "555-3242", "d"));
+
+        List <String> val = people.values().stream()
+                .flatMap(List::stream)
+                .filter(v -> v.matches("[a-zA-Z]"))
+                .sorted()
+                .toList();
+
+        System.out.println("List of letters that only occurs in the Map: " + val);
+
+    }
+}
